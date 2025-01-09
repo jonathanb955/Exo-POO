927ec8c9914fc750ede21bbaaeeb5d5a000bd1f8
Jonathan BAO <BAO_Jo@lprs.local>
2025-01-09 14:52:18 +0100

    Ajout de la classe Ingrédient

---
+++ src/Ingredient.java
@@ -0,0 +1,12 @@
+public class Ingredient {
+
+    private String nomIngredient;
+    private int quantites;
+    private String unite;
+
+    public Ingredient (String nomIngredient, int quantites, String unite) {
+        this.nomIngredient = nomIngredient;
+        this.quantites = quantites;
+        this.unite = unite;
+    }
+}
